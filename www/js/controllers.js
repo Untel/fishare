cdc1de234a449c2108bc89cdecb155ac5105a1f9
Clément Ignacio <ignacio.e1401018@etud.univ-ubs.fr>
2017-01-28 17:43:40 +0100

    Tableau de bord

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,56 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
-  console.log("dash ctrl");
+  $scope.datas = [
+    {
+      'id' : 1,
+      'prenom' : 'Clément',
+      'nom' : 'Ignacio',
+      'date' : '15 Janvier 2017',
+      'pts' : 200,
+      'img_user' : 'img/clem.jpg',
+      'img_photo' : 'img/peche1.jpg',
+      'type' : 'Bar',
+      'poids' : 15,
+      'taille': 120,
+      'lieu' : 'Vannes',
+      'description' : 'Journée incroyable ! Regardez ce magnifique poisson ;)',
+      'like' : 1,
+      'comment' : 5
+    },
+    {
+      'id' : 2,
+      'prenom' : 'Glenn',
+      'nom' : 'Guegan',
+      'date' : '14 Janvier 2017',
+      'pts' : 170,
+      'img_user' : 'img/glenn.jpg',
+      'img_photo' : 'http://www.1max2peche.com/wp-content/uploads/2015/10/carpe-record-france-04.jpg',
+      'type' : 'Carpe',
+      'poids' : 35,
+      'taille': 170,
+      'lieu' : 'Brest',
+      'description' : 'Pas mal, non ?',
+      'like' : 4,
+      'comment' : 2
+    },
+    {
+      'id' : 3,
+      'prenom' : 'Adrien',
+      'nom' : 'Fernandez',
+      'date' : '14 Janvier 2017',
+      'pts' : 20,
+      'img_user' : 'img/adrien.jpg',
+      'img_photo' : 'https://static.pratique.fr/images/unsized/pe/peche-maquereau.jpg',
+      'type' : 'Maquereau',
+      'poids' : 0.3,
+      'taille': 30,
+      'lieu' : 'Cherbourg',
+      'description' : 'Ma seule prise de la journée :/',
+      'like' : 0,
+      'comment' : 0
+    }
+  ];
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
